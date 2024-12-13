6c043aa30d787fa44d046464b7f52788299ba44d
Irsyad-1 <90511162+Irsyad-1@users.noreply.github.com>
2024-12-13 21:45:00 +0700

    include fontawesome css

--- resources/views/layouts/master-front.blade.php
+++ resources/views/layouts/master-front.blade.php
@@ -5,6 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
   @vite(['resources/css/app.css', 'resources/js/app.js'])
   @if (Route::is('home'))
     <title>{{ config('app.name', 'Laravel') }}</title>
