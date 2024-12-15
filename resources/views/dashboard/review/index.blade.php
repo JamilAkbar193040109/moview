7d3acb4829ccefcf83f9a56b152ce1f587d2f40f
lamhotkristopher <Lamhot.kristopher27@gmail.com>
2024-12-15 20:19:50 +0700

    change some style on list review page

--- resources/views/dashboard/review/index.blade.php
+++ resources/views/dashboard/review/index.blade.php
@@ -9,21 +9,28 @@
     </div>
     <div class="card overflow-x-auto rounded-lg border border-gray-500 p-4">
       <div class="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-        <div class="card bg-theme-base w-full rounded-md p-4">
-          @forelse ($reviews as $review)
+        @forelse ($reviews as $review)
+          <div class="card w-full rounded-md bg-modern-accent p-4 dark:bg-modern-text">
             <div class="card-body">
               <h5 class="card-title text-xl font-semibold">{{ $review->movie->judul }} ({{ $review->movie->tahun_rilis }})</h5>
               <h6 class="card-subtitle text-theme-muted mb-2 text-sm">By {{ $review->user->name }} | Rating: {{ $review->rating }}/10</h6>
               <p class="card-text text-sm">{{ Str::limit($review->review_text, 150) }}</p>
               <div class="flex gap-x-2">
                 <a class="mt-3 block w-fit rounded-md bg-sky-600 p-2 text-sm hover:underline" href="{{ route('dashboard.reviews.show', $review->id) }}">Read More</a>
-                <a class="text-theme-base mt-3 block w-fit rounded-md bg-yellow-500 p-2 text-sm hover:underline" href="{{ route('dashboard.reviews.edit', $review->id) }}"><i class="fa-solid fa-pen mr-1"></i> Edit Review</a>
+                <a class="text-modern mt-3 block w-fit rounded-md bg-yellow-500 p-2 text-sm hover:underline" href="{{ route('dashboard.reviews.edit', $review->id) }}"><i class="fa-solid fa-pen mr-1"></i></a>
+                <form action="{{ route('dashboard.reviews.destroy', $review->id) }}" method="post">
+                  @csrf
+                  @method('DELETE')
+                  <button class="mt-3 block w-fit rounded-md bg-red-500 p-2 text-sm hover:underline"><i class="fa-solid fa-trash mr-1"></i></button>
+                </form>
               </div>
             </div>
-          @empty
+          </div>
+        @empty
+          <div class="col-span-3">
             <p class="text-center">Belum ada review</p>
-          @endforelse
-        </div>
+          </div>
+        @endforelse
       </div>
       <div class="mt-4 flex justify-between">
         {{ $reviews->links() }}
