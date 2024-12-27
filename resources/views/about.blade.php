2fe083c50ce9bdf6786a95f33df4362d69877ee4
Irsyad-1 <ichantampan05@gmail.com>
2024-12-27 22:53:30 +0700

    update about page content and improve formatting

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,13 +1,14 @@
 @extends('layouts.master-front')
 
+@section('title', 'About')
 @section('content')
   <section class="about mx-auto max-w-6xl px-4 py-10">
     <h2 class="mb-4 text-2xl font-semibold">About App</h2>
-    <p class="text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, quibusdam.</p>
+    <p class="text-gray-600">Moview adalah sebuah platform yang membantu Anda menemukan film yang sesuai dengan selera Anda. Kami juga memberikan Anda kesempatan untuk berbagi pendapat dengan memberikan review terhadap film yang Anda tonton.</p>
     <p class="mt-4 text-lg font-semibold">
-      Anggota Kelompok
+      Developer Team:
     </p>
-    <ol class="mt-2 list-decimal pl-4 leading-relaxed">
+    <ol class="mt-2 list-inside list-decimal leading-relaxed">
       <li>Jamil Yohanes Akbar Naiborhu (193040109)</li>
       <li>Muhammad Irsyad Hibatullah (193040097)</li>
       <li>Lamhot Kristopher (193040007)</li>
